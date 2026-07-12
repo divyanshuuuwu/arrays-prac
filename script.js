3f7a223109678967fb4ca76723f53d7c4257ecc2
divyanshu <divyanshusah44@gmail.com>
2026-07-12 16:19:04 +0530

    finding the greatest num in an array

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-let arr = [20 , 50, 40];
-let sum = 0;
-for (let i = 0; i < arr.length; i++) {
-    sum = sum + arr[i]
+// sum of array
+
+// let arr = [20 , 50, 40];
+// let sum = 0;
+// for (let i = 0; i < arr.length; i++) {
+//     sum = sum + arr[i]
+// }
+// console.log(sum)
+
+// finding the max or greatest element in our array
+
+
+let arr = [20, 40, 80, 5, 8,];
+let max = arr[0];
+for(let i = 1; i<arr.length; i++){
+    if(arr[i]>max){
+        max = arr[i]
+    }
 }
-console.log(sum)
+console.log(max)
