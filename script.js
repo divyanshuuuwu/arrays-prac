f8075e2ccf3d14aab1331fffcabf15d4a34ec853
divyanshu <divyanshusah44@gmail.com>
2026-07-12 16:28:02 +0530

    find the smallest

--- script.js
+++ script.js
@@ -7,14 +7,27 @@
 // }
 // console.log(sum)
 
+
+
+
 // finding the max or greatest element in our array
 
+// let arr = [20, 40, 80, 5, 8,];
+// let max = arr[0];
+// for(let i = 1; i<arr.length; i++){
+//     if(arr[i]>max){
+//         max = arr[i]
+//     }
+// }
+// console.log(max)
 
-let arr = [20, 40, 80, 5, 8,];
-let max = arr[0];
-for(let i = 1; i<arr.length; i++){
-    if(arr[i]>max){
-        max = arr[i]
-    }
+
+// find the smallest number in an array
+
+let arr = [60, 50, 100, 20, 80];
+let min = arr[0];
+for(let i=1; i<arr.length; i++){
+    if(min > arr[i])
+        min = arr[i]
 }
-console.log(max)
+console.log(min)
