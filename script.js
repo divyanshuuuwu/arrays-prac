7173f483c6ecd2d4cc0f52b9b49830b22eb7aa20
divyanshu <divyanshusah44@gmail.com>
2026-07-13 00:43:26 +0530

    rev an array without extra space

--- script.js
+++ script.js
@@ -75,14 +75,27 @@
 
 
 // reverse an array with an extra space 
+// let arr = [20, 30, 40, 50, 60];
+// let newArr = new Array(arr.length);
 
-let arr = [20, 30, 40, 50, 60];
-let newArr = new Array(arr.length);
-
-let j = 0;
-for(let i = arr.length-1; i>=0; i--){
-    newArr[j] = arr[i];
-    j++
+// let j = 0;
+// for(let i = arr.length-1; i>=0; i--){
+//     newArr[j] = arr[i];
+//     j++
     
+// }
+// console.log(newArr)
+
+// reverse an array without usiing extra space
+let arr = [20, 30, 40, 50, 60];
+let i = 0
+let j = arr.length-1
+ 
+while(i!=j){
+    let temp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = temp
+    i++
+    j--
 }
-console.log(newArr)
+console.log(arr)
