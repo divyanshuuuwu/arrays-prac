50fe0eda6d4773707da966d7f58980019b4c4fda
divyanshu <divyanshusah44@gmail.com>
2026-07-15 01:05:57 +0530

    right rotataion by 1 element

--- script.js
+++ script.js
@@ -118,11 +118,22 @@
 
  // left rotation by 1 element
 
- let arr = [1, 2, 3, 4, 5]
- let copy = arr[0];
- for(let i = 0; i<arr.length-1; i++){
-    arr[i] = arr[i+1]
- }
- arr[arr.length-1]=copy
- console.log(arr);
+//  let arr = [1, 2, 3, 4, 5]
+//  let copy = arr[0];
+//  for(let i = 0; i<arr.length-1; i++){
+//     arr[i] = arr[i+1]
+//  }
+//  arr[arr.length-1]=copy
+//  console.log(arr);
  
+// right rotataion by 1 element
+
+let arr = [1, 2, 3, 4, 5]
+let copy = arr[arr.length-1];
+
+for(let i= arr.length-1; i>0; i--){
+    arr[i] = arr[i-1]
+}
+arr[0] = copy
+console.log(arr);
+
