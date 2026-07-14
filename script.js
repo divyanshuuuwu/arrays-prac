aa173cf11067e6aed1f69f052411b0d7dd66048a
divyanshu <divyanshusah44@gmail.com>
2026-07-15 00:33:28 +0530

    left rotation by 1 element

--- script.js
+++ script.js
@@ -100,18 +100,29 @@
 // }
 // console.log(arr)
 
-//zero on one side
-let nums = [0,1,0,3,12]
-  let i = 0;
-let j = 0;
+//zero on one side 
+// let nums = [0,1,0,3,12]
+//   let i = 0;
+// let j = 0;
 
-while(i < nums.length){
-    if(nums[i] !== 0){
-        let temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-        j++;
-    }
-    i++;
-}
-console.log(nums)
+// while(i < nums.length){
+//     if(nums[i] !== 0){
+//         let temp = nums[i];
+//         nums[i] = nums[j];
+//         nums[j] = temp;
+//         j++;
+//     }
+//     i++;
+// }
+// console.log(nums)
+
+ // left rotation by 1 element
+
+ let arr = [1, 2, 3, 4, 5]
+ let copy = arr[0];
+ for(let i = 0; i<arr.length-1; i++){
+    arr[i] = arr[i+1]
+ }
+ arr[arr.length-1]=copy
+ console.log(arr);
+ 
