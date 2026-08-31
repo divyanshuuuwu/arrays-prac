24efe1da883b5d5d7f7ffe0cb6e6d042ee4b6f54
divyanshu <divyanshusah44@gmail.com>
2026-08-31 23:57:08 +0530

    // Roatate an array right with extra space // Roatate an array right with extra space

--- script.js
+++ script.js
@@ -202,12 +202,12 @@
 
 // two sum my Brute force  
 
-let nums = [2,7,11,15]
-let target = 9
-// output: [0,1]
-for(let i =0; i<nums.length; i++){
-        for(let j = i+1; j<nums.length; j++)
-        if(nums[i] + nums[j] === target){
-            console.log([i,j])
-        }
-    }
+// let nums = [2,7,11,15]
+// let target = 9
+// // output: [0,1]
+// for(let i =0; i<nums.length; i++){
+//         for(let j = i+1; j<nums.length; j++)
+//         if(nums[i] + nums[j] === target){
+//             console.log([i,j])
+//         }
+//     }
