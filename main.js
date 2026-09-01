3a7a5631f94c90baebd0fededa1ba606483e7316
divyanshu <divyanshusah44@gmail.com>
2026-09-01 23:08:45 +0530

    // Check if All A's Appears Before All B's

--- main.js
+++ main.js
@@ -11,25 +11,38 @@
 //         nums[i] = temp[i] }
 //         console.log(nums)
 // Rotate an Array to right Optimal
-let nums = [1, 2, 3, 4, 5, 6];
-let k = 3;
-k = k % nums.length;
-for (let i = 0, j = nums.length - 1; i <= j; i++) {
-    let temp = nums[j];
-    nums[j] = nums[i];
-    nums[i] = temp;
-    j--;
+// let nums:number[] = [1,2,3,4,5,6]
+// let k:number = 3
+//  k = k % nums.length;
+//    for(let i:number = 0, j = nums.length-1; i<=j; i++){
+//         let temp:number = nums[j]
+//         nums[j] = nums[i]
+//         nums[i] = temp
+//         j--
+//    }
+//     for(let i:number = 0, j = k-1; i<=j; i++){
+//         let temp:number = nums[j]
+//         nums[j] = nums[i]
+//         nums[i] = temp
+//         j--
+//     }
+//      for(let i:number = k, j = nums.length-1; i<=j; i++){
+//         let temp:number = nums[j]
+//         nums[j] = nums[i]
+//         nums[i] = temp
+//         j--
+//     }
+//     console.log(nums)
+// Check if All A's Appears Before All B's
+const s = "abab";
+function checkString(s) {
+    for (let i = 0; i < s.length - 1; i++) {
+        if (s[i] === "b" && s[i + 1] === "a") {
+            return false;
+        }
+    }
+    return true;
 }
-for (let i = 0, j = k - 1; i <= j; i++) {
-    let temp = nums[j];
-    nums[j] = nums[i];
-    nums[i] = temp;
-    j--;
-}
-for (let i = k, j = nums.length - 1; i <= j; i++) {
-    let temp = nums[j];
-    nums[j] = nums[i];
-    nums[i] = temp;
-    j--;
-}
-console.log(nums);
+;
+const result = checkString(s);
+console.log(result);
--- main.ts
+++ main.ts
@@ -17,30 +17,48 @@
 
 
 // Rotate an Array to right Optimal
-let nums:number[] = [1,2,3,4,5,6]
-let k:number = 3
+// let nums:number[] = [1,2,3,4,5,6]
+// let k:number = 3
 
- k = k % nums.length;
-   for(let i:number = 0, j = nums.length-1; i<=j; i++){
-        let temp:number = nums[j]
-        nums[j] = nums[i]
-        nums[i] = temp
-        j--
-   }
+//  k = k % nums.length;
+//    for(let i:number = 0, j = nums.length-1; i<=j; i++){
+//         let temp:number = nums[j]
+//         nums[j] = nums[i]
+//         nums[i] = temp
+//         j--
+//    }
     
-    for(let i:number = 0, j = k-1; i<=j; i++){
-        let temp:number = nums[j]
-        nums[j] = nums[i]
-        nums[i] = temp
-        j--
-    }
+//     for(let i:number = 0, j = k-1; i<=j; i++){
+//         let temp:number = nums[j]
+//         nums[j] = nums[i]
+//         nums[i] = temp
+//         j--
+//     }
 
-     for(let i:number = k, j = nums.length-1; i<=j; i++){
-        let temp:number = nums[j]
-        nums[j] = nums[i]
-        nums[i] = temp
-        j--
-    }
+//      for(let i:number = k, j = nums.length-1; i<=j; i++){
+//         let temp:number = nums[j]
+//         nums[j] = nums[i]
+//         nums[i] = temp
+//         j--
+//     }
 
-    console.log(nums)
+//     console.log(nums)
       
+
+
+// Check if All A's Appears Before All B's
+const s = "abab"
+
+function checkString(s: string): boolean {
+    for(let i:number = 0; i<s.length-1; i++){
+        if(s[i] === "b" && s[i+1] === "a"){
+
+            return false
+        }
+
+    }
+
+    return true
+};
+const result = checkString(s)
+console.log(result)
